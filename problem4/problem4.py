de995e5987203ae78416f91a5abada04168465c4
jysru <jysru@pm.me>
2022-12-04 09:35:27 +0100

    Update including problem4 2-2

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -1,20 +1,17 @@
-if __name__ == "__main__":
-    filename = "test_input.txt"
-    filename = "puzzle_input.txt"
+filename = "puzzle_input.txt"
 
-    # File processing
-    with open(filename) as f:
-        lines = f.read().splitlines()
-        lines = [line.replace("-", ",").split(",") for line in lines]
+with open(filename) as f:
+    lines = f.read().splitlines()
+    lines = [line.replace("-", ",").split(",") for line in lines]
 
-    # Sets processing
-    fully_contained_pairs = 0
-    for idx, line in enumerate(lines):
-        sect0 = set(range(int(line[0]), int(line[1])+1))
-        sect1 = set(range(int(line[2]), int(line[3])+1))
-        sub0, sup0 = sect0.issubset(sect1), sect0.issuperset(sect1)
-        if any([sub0, sup0]):
-            fully_contained_pairs += 1
+fully_contained_pairs, overlapped_pairs = 0, 0
+for idx, line in enumerate(lines):
+    sect0 = set(range(int(line[0]), int(line[1])+1))
+    sect1 = set(range(int(line[2]), int(line[3])+1))
+    sub0, sup0 = sect0.issubset(sect1), sect0.issuperset(sect1)
+    if any([sub0, sup0]):
+        fully_contained_pairs += 1
+    if not sect0.isdisjoint(sect1):
+        overlapped_pairs += 1
 
-    print(fully_contained_pairs)
-
+print(f"Fully contained: {fully_contained_pairs}, overlaps: {overlapped_pairs}")
