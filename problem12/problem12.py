8475a78160fb02e6e0dc24c64c95211a78d0c524
jysru <jysru@pm.me>
2023-01-13 17:46:32 +0100

    Started finding_path

--- problem12/problem12.py
+++ problem12/problem12.py
@@ -2,9 +2,10 @@ from queue import PriorityQueue
 import numpy as np
 
 
-class Graph:
+class GraphParser:
     start_height = 0
-    stop_height = 0
+    stop_height = 25
+    ref = ord('a')
 
     def __init__(self, file: str) -> None:
         self.file: str = file
@@ -38,7 +39,14 @@ class Graph:
 
 
     def __convert(self) -> None:
-        pass
+        for row, line in enumerate(self.height):
+            for col, char in enumerate(line):
+                if char == 'S':
+                    self.height[row][col] = GraphParser.start_height
+                elif char == 'E':
+                    self.height[row][col] = GraphParser.stop_height
+                else:
+                    self.height[row][col] = ord(char) - GraphParser.ref
 
 
     @property
@@ -57,9 +65,91 @@ class Graph:
         - Stop coordinates: {self.stop}
         - Map: {self.height}
         """
+    
+
+class GraphRunner:
+
+    def __init__(self, parse: GraphParser) -> None:
+        self.height: list[list[int]] = parse.height
+        self.start: tuple[int, int] = parse.start
+        self.stop: tuple[int, int] = parse.stop
+        self.current: tuple[int, int] = parse.start
+        self.visited: list[tuple[int, int, int]] = [parse.start]
+        self.neighbours: list[tuple[int, int, int]] = [None, None, None, None] # Top, Left, Bottom, Right
+
+
+    def find_path(self):
+        self.get_neighbours()
+        valid_neighbours = []
+        for i, neighbour in enumerate(self.neighbours):
+            if neighbour:
+                if neighbour[2] in [self.current_value, self.current_value+1]:
+                    valid_neighbours.append(neighbour)
+
+
+
+
+    def get_neighbours(self):
+        self.neighbours = [self.__get_top(), self.__get_left(), self.__get_bottom(), self.__get_right()]
+
+    def __get_top(self) -> tuple[int, int, int]:
+        if self.current[0] == 0: # We are at the top edge
+            return None
+        else:
+            x, y = self.current[0]-1, self.current[1]
+            return (x, y, self.height[x][y])
+    
+    def __get_bottom(self) -> tuple[int, int, int]:
+        if self.current[0] == self.size[0]: # We are at the bottom edge
+            return None
+        else:
+            x, y = self.current[0]+1, self.current[1]
+            return (x, y, self.height[x][y])
+        
+    def __get_left(self) -> tuple[int, int, int]:
+        if self.current[1] == 0: # We are at the left edge
+            return None
+        else:
+            x, y = self.current[0], self.current[1]-1
+            return (x, y, self.height[x][y])
+    
+    def __get_right(self) -> tuple[int, int, int]:
+        if self.current[1] == self.size[1]: # We are at the right edge
+            return None
+        else:
+            x, y = self.current[0], self.current[1]+1
+            return (x, y, self.height[x][y])
+
+    @property
+    def size(self) -> tuple[int, int]:
+        if self.height:
+            return (len(self.height), len(self.height[0]))
+        else:
+            return None
+        
+    @property
+    def current_value(self):
+        return self.height[self.current[0]][self.current[1]]
+        
+        
+    def __str__(self) -> str:
+        return f"""
+        Graph:
+        - Size: {self.size[0]} x {self.size[1]}
+        - Start coordinates: {self.start}
+        - Stop coordinates: {self.stop}
+        - Current coordinate: {self.current}
+        - Current value: {self.current_value}
+        - Neighbours coordinates: {self.neighbours}
+        - Number of visits: {len(self.visited)}
+        """
+        
+
 
 
 if __name__ == "__main__":
     file = "test_input.txt"
-    graph = Graph(file=file)
+    parse = GraphParser(file=file)
+    graph = GraphRunner(parse=parse)
+    graph.get_neighbours()
     print(graph)
