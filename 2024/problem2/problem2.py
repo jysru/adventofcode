2935505fc0fc081e2b56f5ef26ae1c3b5723fa40
jysru <jysru@pm.me>
2024-12-02 10:51:25 +0100

    Added part1

--- 2024/problem2/problem2.py
+++ 2024/problem2/problem2.py
@@ -1,22 +1,40 @@
 import numpy as np
 
 file = 'example.txt'
-file = 'puzzle.txt'
+# file = 'puzzle.txt'
 
 def report2array(report: str) -> np.ndarray:
-    return np.array(list(report.rstrip().replace(' ', ''))).astype(int)
+    return np.array(report.rstrip().split(' ')).astype(int)
 
 def is_safe(report: np.ndarray) -> bool:
     diff = np.diff(report)
     safe = True
-    print(diff)
-    if np.any(np.sign(diff) != np.sign(diff[0])):
+    ref_sign = np.sign(diff[0]) if np.sign(diff[0]) != 0 else 1
+    if np.any(np.sign(diff) != ref_sign):
         safe = False
-    elif np.any(np.abs(diff) < 1) or np.any(np.abs(diff) > 3):
+    elif np.any(np.abs(diff) < 1):
         safe = False
+    elif np.any(np.abs(diff) > 3):
+        safe = False
+    print(diff, safe)
     return safe
     
 
+def is_dampened_safe(report: np.ndarray) -> bool:
+    diff = np.diff(report)
+    safe = True
+
+    ref_sign = np.sign(diff[0]) if np.sign(diff[0]) != 0 else 1
+    signs = np.sign(diff) != ref_sign
+    down_levels = np.abs(diff) < 1
+    up_levels = np.abs(diff) > 3
+    sum_checks = np.logical_or(np.logical_or(signs, down_levels), up_levels)
+    print(np.sum(sum_checks))
+    
+    if np.sum(sum_checks) > 1:
+        safe = False
+    print(diff, safe)
+    return safe
 
 
 if __name__ == '__main__':
@@ -25,7 +43,6 @@ if __name__ == '__main__':
     
     safe_reports = 0
     for report in reports:
-        safe_reports += is_safe(report2array(report))
-        print(is_safe(report2array(report)))
+        safe_reports += is_dampened_safe(report2array(report))
 
     print(safe_reports)
