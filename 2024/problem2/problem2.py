f56509b7e871f94fa05f5d065e47e39c80227b0f
jysru <jysru@pm.me>
2024-12-02 11:01:22 +0100

    Update problem2.py

--- 2024/problem2/problem2.py
+++ 2024/problem2/problem2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 file = 'example.txt'
-# file = 'puzzle.txt'
+file = 'puzzle.txt'
 
 def report2array(report: str) -> np.ndarray:
     return np.array(report.rstrip().split(' ')).astype(int)
@@ -16,25 +16,12 @@ def is_safe(report: np.ndarray) -> bool:
         safe = False
     elif np.any(np.abs(diff) > 3):
         safe = False
-    print(diff, safe)
     return safe
     
 
 def is_dampened_safe(report: np.ndarray) -> bool:
-    diff = np.diff(report)
-    safe = True
-
-    ref_sign = np.sign(diff[0]) if np.sign(diff[0]) != 0 else 1
-    signs = np.sign(diff) != ref_sign
-    down_levels = np.abs(diff) < 1
-    up_levels = np.abs(diff) > 3
-    sum_checks = np.logical_or(np.logical_or(signs, down_levels), up_levels)
-    print(np.sum(sum_checks))
-    
-    if np.sum(sum_checks) > 1:
-        safe = False
-    print(diff, safe)
-    return safe
+    safes = [is_safe(np.delete(report, i)) for i in range(len(report))]
+    return True if np.any(safes) else False
 
 
 if __name__ == '__main__':
@@ -42,7 +29,10 @@ if __name__ == '__main__':
         reports = f.readlines()
     
     safe_reports = 0
+    safe_dampened_reports = 0
     for report in reports:
-        safe_reports += is_dampened_safe(report2array(report))
+        safe_reports += is_safe(report2array(report))
+        safe_dampened_reports += is_dampened_safe(report2array(report))
 
     print(safe_reports)
+    print(safe_dampened_reports)
