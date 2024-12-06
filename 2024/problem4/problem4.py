dfe251f2627b718b7198f5293071dde23d36e348
jysru <jysru@pm.me>
2024-12-07 00:46:25 +0100

    Added part 2

--- 2024/problem4/problem4.py
+++ 2024/problem4/problem4.py
@@ -21,13 +21,10 @@ def pad_array(data: np.ndarray, word: list) -> np.ndarray:
     return data
 
 
-def word_in_list(data: np.ndarray, word: np.ndarray, add_reverse: bool = True) -> int:
+def word_in_list(data: np.ndarray, word: np.ndarray) -> int:
     count = 0
     if np.all(data.flatten() == word.flatten()):
         count += 1
-    if add_reverse:
-        if np.all(data.flatten() == np.flip(word).flatten()):
-            count += 1
     return count
 
 
@@ -48,19 +45,36 @@ def get_vector(data: np.ndarray, coords: tuple[int, int], word: list, direction:
     if direction == "dl":
         matrix = data[row:(row+len(word)), (col-(len(word) - 1)):(col+1)]
         vector = np.diag(np.fliplr(matrix))
-
     if direction == "ur":
         matrix = data[(row-(len(word) - 1)):(row+1), col:(col+len(word))]
         vector = np.diag(np.flipud(matrix))
-    
     if direction == "ul":
         matrix = data[(row-(len(word) - 1)):(row+1), (col-(len(word) - 1)):(col+1)]
         vector = np.flip(np.diag(matrix))
-    
     else:
         ValueError('Invalid direction')
 
     return vector
+
+
+def check_xmas_cross(data: np.ndarray, coords: tuple[int, int]) -> int:
+    row, col = coords
+
+    center = data[row, col]
+    if center != 'A':
+        return 0
+    else:
+        top_left = data[row - 1, col - 1]
+        bottom_right = data[row + 1, col + 1]
+        if (top_left == 'M' and bottom_right == 'S') or (top_left == 'S' and bottom_right == 'M'):
+            top_right = data[row - 1, col + 1]
+            bottom_left = data[row + 1, col - 1]
+            if (top_right == 'M' and bottom_left == 'S') or (top_right == 'S' and bottom_left == 'M'):
+                return 1
+            else:
+                return 0
+        else:
+            return 0
 
 
 
@@ -70,19 +84,16 @@ if __name__ == "__main__":
     print(data)
     
     directions = ["r", "d", "l", "u", "dr", "dl", "ur", "ul"]
-    # directions = ["ul"]
 
-    counter = 0
+    xmas_counter = 0
+    xmas_cross_counter = 0
     for i_row in range(len(word) - 1, data.shape[0] - (len(word) - 1)):
         for i_col in range(len(word) - 1, data.shape[1] - (len(word) - 1)):
+            xmas_cross_counter += check_xmas_cross(data, (i_row, i_col))
+
             for direction in directions:
                 vector = get_vector(data, (i_row, i_col), word, direction)
-                if i_row == 4:
-                    print(i_row, i_col, vector)
-                counter += word_in_list(vector,  word, add_reverse=False)
+                xmas_counter += word_in_list(vector, word, add_reverse=False)
             
-    print(counter)
-
-    # print(data)
-    # print(data[0][1] * 3)
-
+    print(xmas_counter)
+    print(xmas_cross_counter)
